035b50f1a4f926d311f791299d18dc954de6b677
YannLeBoudec <finalsmashganondorf@gmail.com>
2020-04-29 11:04:24 +0200

    Test index
    test

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+require_once 'includes/header.php';
+use Carbon\Carbon;
+
+connectDB();
+
+?>
+
+<section>
+    <div class="container">
+            test
+    </div>
+</section>
+
+<?php require_once 'includes/footer.php'; ?>
