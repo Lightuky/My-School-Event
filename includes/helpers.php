6c194ce20ac5c4f37508226e49e7ff01e9fb5b35
YannLeBoudec <finalsmashganondorf@gmail.com>
2020-04-29 17:20:01 +0200

    Added friendship functions
    + get user(s) functions

--- includes/helpers.php
+++ includes/helpers.php
@@ -56,3 +56,67 @@ function authUser($data) {
 function authOut() {
     session_destroy();
 }
+
+function getUser($id) {
+    $dbh = connectDB();
+    $stmt = $dbh->prepare("SELECT * FROM users WHERE id = :id LIMIT 1");
+    $stmt->bindValue(':id', $id);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function getUsers($id) {
+    $dbh = connectDB();
+    $stmt = $dbh->prepare("SELECT * FROM users WHERE id != $id");
+    $stmt->execute();
+    return $stmt->fetchAll(PDO::FETCH_ASSOC);
+}
+
+function checkFriend($auth_id, $user2) {
+    $dbh = connectDB();
+    $stmt = $dbh->prepare("SELECT * FROM friends WHERE (user1_id = :user1_id AND user2_id = :user2_id) OR (user1_id = :user2_id AND user2_id = :user1_id)");
+    $stmt->bindValue(':user1_id', $auth_id);
+    $stmt->bindValue(':user2_id', $user2);
+    $stmt->execute();
+    return $stmt->fetch(PDO::FETCH_ASSOC);
+}
+
+function addFriend($pending, $auth_id, $user2_id) {
+    $dbh = connectDB();
+    $stmt = $dbh->prepare( "INSERT INTO friends (user1_id, user2_id, pending) VALUES (:user1_id, :user2_id, :pending)");
+    $stmt->bindValue(':user1_id', $auth_id);
+    $stmt->bindValue(':user2_id', $user2_id);
+    $stmt->bindValue(':pending', $pending);
+    $stmt->execute();
+}
+
+function acceptFriendRequest($pending, $auth_id, $user2_id) {
+    $dbh = connectDB();
+    $stmt = $dbh->prepare("UPDATE friends SET pending = :pending WHERE user1_id = :user1_id AND user2_id = :user2_id");
+    $stmt->bindValue(':pending', $pending);
+    $stmt->bindValue(':user1_id', $user2_id);
+    $stmt->bindValue(':user2_id', $auth_id);
+    $stmt->execute();
+}
+
+function deleteFriend($pending, $auth_id, $user2_id) {
+    $dbh = connectDB();
+    $stmt = $dbh->prepare("DELETE FROM friends WHERE (user1_id = :user1_id AND user2_id = :user2_id AND pending = :pending) OR (user1_id = :user2_id AND user2_id = :user1_id AND pending = :pending)");
+    $stmt->bindValue(':pending', $pending);
+    $stmt->bindValue(':user1_id', $auth_id);
+    $stmt->bindValue(':user2_id', $user2_id);
+    $stmt->execute();
+}
+
+function updateUser($data, $id) {
+    $dbh = connectDB();
+    $stmt = $dbh->prepare("UPDATE users SET last_name = :last_name, first_name = :first_name, email = :email, phone_number = :phone_number, birthday = :birthday, gender = :gender, updated_at = :updated_at WHERE id = $id");
+    $stmt->bindValue(':last_name', $data['last_name']);
+    $stmt->bindValue(':first_name', $data['first_name']);
+    $stmt->bindValue(':email', $data['email']);
+    $stmt->bindValue(':phone_number', $data['phone_number']);
+    $stmt->bindValue(':birthday', $data['birthday']);
+    $stmt->bindValue(':gender', $data['gender']);
+    $stmt->bindValue(':updated_at', date("Y-m-d H:i:s", time()));
+    $stmt->execute();
+}
