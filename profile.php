bbe3d9594847b6bd7c2c0d83a43b94874c87d326
antoinemourat <antoinemourat@gmail.com>
2020-05-05 10:06:24 +0200

    scss profile

--- profile.php
+++ profile.php
@@ -16,12 +16,15 @@ if (isset($_SESSION['auth_id'])) {
 }
 
 ?>
-    <section class="linear-gradient  h-50">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-4 text-center mt-3">
-                    <img src="https://www.gravatar.com/avatar/<?php echo md5($user['email']); ?>?s=600" alt="" class="m-auto d-block shadow border border-white rounded-circle w-25">
-                    <strong class="text-white small mt-3 ">Inscrit <?php echo getDateForHumans($user['date_added']); ?></strong>
+    <section class="section-up"></section>
+
+    <div class="section-flex">
+    <section class="section-down">
+        <div class="container d-flex align-content-center">
+            <div class="">
+                <div class="text-center">
+                    <img src="https://www.gravatar.com/avatar/<?php echo md5($user['email']); ?>?s=600" alt="" class="d-profile-picture m-auto d-block shadow border border-white rounded-circle w-25">
+                    <strong class="small">Inscrit <?php echo getDateForHumans($user['date_added']); ?></strong>
                     <div class="container">
                         <?php if (isset($_SESSION['auth_id'])) {
                             if ($id == $_SESSION['auth_id']) { ?>
@@ -37,7 +40,7 @@ if (isset($_SESSION['auth_id'])) {
                             <?php else: ?>
                                 <?php if ($friend['pending'] === '2'): ?>
                                     <div class="d-flex mb-2">
-                                        <div class="btn bg-success text-white">Déja Amis</div>
+                                        <div class="btn bg-success">Déja Amis</div>
                                         <div class="ml-4"><a href="assets/friends.php?s=2&id=<?php echo $id ?>" class="btn btn-danger">Supprimer l'ami</a></div>
                                     </div>
                                     <span>(Ajouté en ami <?php echo getDateForHumans($friend['date_added']); ?>)</span>
@@ -55,14 +58,14 @@ if (isset($_SESSION['auth_id'])) {
                     <?php endif; ?>
                     </div>
                 </div>
-                <div class="col-lg-8">
-                    <div class="text-white text-center">
+                <div class="">
+                    <div class=" text-center">
 
                         <h1 class="font-size-name-profile" ><?php echo $user['first_name'] . " " . $user['last_name'] ?></h1>
 
-                    </div>
+                    <!--</div>
                     <div class="mt-5 h3 text-muted">Événements gérés :</div>
-                    <div>
+                    <div>-->
                         <?php foreach ($owned_events as $owned_event) { ?>
                             <a class="nav-link card col-3 p-3 text-center mt-5 d-flex" href="event.php?id=<?php echo $owned_event['id'] ?>"><?php echo $owned_event['name'] ?></a>
                         <?php } ?>
@@ -71,10 +74,12 @@ if (isset($_SESSION['auth_id'])) {
             </div>
         </div>
         <div class="d-flex justify-content-center mt-2 pb-5">
-            <span class="fs-info-profile pr-2 border-right  text-white d-block"><?php echo $user['school_year'] ?>°</span>
-            <span class="fs-info-profile pr-2 pl-2 text-white border-right d-block"><?php echo $user_school['name'] ?></span>
-            <span class="fs-info-profile pl-2 text-white d-block"><?php echo $user['email'] ?></span>
+            <span class="fs-info-profile pr-2 border-right d-block"><?php echo $user['school_year'] ?>°</span>
+            <span class="fs-info-profile pr-2 pl-2  border-right d-block"><?php echo $user_school['name'] ?></span>
+            <span class="fs-info-profile pl-2 d-block"><?php echo $user['email'] ?></span>
         </div>
     </section>
+    <section class="section-feed"></section>
+    <div>
 
 <?php require_once 'includes/footer.php'; ?>
