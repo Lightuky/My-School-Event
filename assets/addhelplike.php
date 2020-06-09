d4371ee902e590952c6554421292ff8c406d49d9
YannLeBoudec <finalsmashganondorf@gmail.com>
2020-06-09 10:03:28 +0200

    fix referers with likes

--- assets/addhelplike.php
+++ assets/addhelplike.php
@@ -2,7 +2,6 @@
 
 require_once '../includes/helpers.php';
 $help_id = isset($_GET['id']) ? $_GET['id'] : null;
-$help_referer = isset($_GET['s']) ? $_GET['s'] : null;
 
 $data = [];
 $fields = [];
@@ -14,24 +13,12 @@ if ($errored) {
     session_start();
     $_SESSION['fields'] = $fields;
 
-    if ($help_referer === '1') {
-        $pathError =  '/index.php?errored=true';
-        header('Location: '. $pathError);
-    }
-    elseif($help_referer === '2') {
-        $pathError =  "/mse/help.php?id=" . $help_id . "&errored=true";
-        header('Location: '. $pathError);
-    }
+    $pathError = $_SERVER['HTTP_REFERER'];
+    header("Location: $pathError");
 }
 else {
     addHelpLike($help_id, $_SESSION['auth_id']);
 
-    if ($help_referer === '1') {
-        $pathSuccess =  "/mse/index.php";
-        header('Location: '. $pathSuccess);
-    }
-    elseif($help_referer === '2') {
-        $pathSuccess =  "/mse/help.php?id=" . $help_id;
-        header('Location: '. $pathSuccess);
-    }
+    $pathSuccess = $_SERVER['HTTP_REFERER'];
+    header("Location: $pathSuccess");
 }
